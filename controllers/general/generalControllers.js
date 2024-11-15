a661fc08c988a5d98f87177b2b0566fec148f602
BaltiBlast <fougeray.florian@gmail.com>
2024-11-15 11:35:24 +0100

    try fix puppeteer chrome issue on render

--- controllers/general/generalControllers.js
+++ controllers/general/generalControllers.js
@@ -9,7 +9,9 @@ const generalControllers = {
     try {
       const { htmlContent, clientEmail, userEmail, date, userName } = req.body;
 
-      const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
       const page = await browser.newPage();
 
       await page.setContent(`
